91ddad2669cb1fc899044a4bf5bc1cbc6b75f5d9
Saul-SF <saulsf1856@gmail.com>
2026-05-08 09:41:30 -0600

    Commit final de laboral

--- informativa-mecatronica/src/app/laboral/laboral.page.ts
+++ informativa-mecatronica/src/app/laboral/laboral.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonFooter } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonButton, IonFooter, IonCard } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-laboral',
   templateUrl: './laboral.page.html',
   styleUrls: ['./laboral.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonFooter , CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonButton, IonFooter, IonCard, CommonModule, FormsModule]
 })
 export class LaboralPage implements OnInit {
 
